31e96a652161b7c115bf2b078a7adc9f41088b54
Finley <altimagewritingco@gmail.com>
2020-03-27 01:18:07 +0100

    Create outbreak.sol

---
+++ strategy/outbreak.sol
@@ -0,0 +1,15 @@
+// contract frame for "patient zero to outbreak"
+
+// three game levels
+// 1. individuals
+// 2. businesses
+// 3. governments
+
+// How does the government efforts to stem an outbreak effect business and individual behaviors?
+// How do business and individual decisions help/hurt spread?
+
+
+User chooses role: peep, biz, gov
+- If role peep, then subject to work and political restrictions
+- If role biz, then subject to employee, client, and political restrictions
+- If role gov, then subject to civil unrest and business restrictions (self-imposed vs. biz disobedience?)
