2e714de1dc4e92405b32c64cf43f080178cac268
Finley <altimagewritingco@gmail.com>
2020-03-29 05:27:29 +0200

    Update quarantine.js

--- strategy/js/quarantine.js
+++ strategy/js/quarantine.js
@@ -1,21 +1,39 @@
-var citizen, no_virus, patient, virus, Recovered, Fatal;
+var no_virus, virus, doctor, patient, Fatal, Recovered;
 
 
-if ('Cases Nearby' == no_virus) {
-  citizen = 'Monitor Situation';
-  window.alert('Disseminate info to Health Care Providers');
-}
-if ('Monitor Situation' == virus) {
-  patient = virus;
-  virus = (typeof virus == 'number' ? virus : 0) + 1;
-  window.alert('Follow Health Care Providers Instructions');
-}
-if ('Quarantine Test' == no_virus) {
-  virus = 'Recovered';
-  Recovered = (typeof Recovered == 'number' ? Recovered : 0) + 1;
-  window.alert('Alert Recovery');
-} else if ('Quarantine Test' == virus) {
-  virus = 'Fatal';
-  Fatal = (typeof Fatal == 'number' ? Fatal : 0) + 1;
-  window.alert('Alert Fatality');
+for (var count = 0; count < 100; count++) {
+  no_virus = '#33cc00';
+  no_virus = 1000;
+  virus = '#ff0000';
+  virus = 1;
+  if ('Cases Nearby' == no_virus) {
+    doctor = 'Monitor Situation';
+    patient = (typeof patient == 'number' ? patient : 0) + 0;
+    patient = '#33cc00';
+    window.alert('Disseminate info to Health Care Providers');
+    break;
+  } else if ('Cases Nearby' == virus) {
+    doctor = 'Monitor Patient';
+    virus = patient;
+    patient = (typeof patient == 'number' ? patient : 0) + 1;
+    patient = '#ff9900';
+    window.alert('New Patient Quarantined ');
+    break;
+  }
+  if ('Quarantine Test' == no_virus) {
+    doctor = 'Monitor Situation';
+    patient = 'Recovered';
+    patient = (typeof patient == 'number' ? patient : 0) + -1;
+    Recovered = (typeof Recovered == 'number' ? Recovered : 0) + 1;
+    Recovered = '#99ff99';
+    window.alert('Alert Recovery');
+    break;
+  } else if ('Quarantine Test' == virus) {
+    patient = Fatal;
+    Fatal = (typeof Fatal == 'number' ? Fatal : 0) + 1;
+    Fatal = '#330033';
+    window.alert('Alert Fatality');
+    break;
+  }
+  break;
 }
